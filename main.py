4a3ef86194747e6f80f250982c8de53930545c65
KwaiSeng <kslai@cisco.com>
2020-09-15 18:02:32 +0800

    Update FLASK_RUN_PORT any

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ def upload_post():
     return redirect(url_for('upload'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=FLASK_RUN_PORT)
+    app.run(host='0.0.0.0', port = FLASK_RUN_PORT)
 
 
 def create_app():
