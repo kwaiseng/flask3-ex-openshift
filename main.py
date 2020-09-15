f358ffedf1df84f3bfcfabeca8149dffa8054feb
KwaiSeng <kslai@cisco.com>
2020-09-15 18:07:40 +0800

    Update FLASK_RUN_PORT any 2

--- main.py
+++ main.py
@@ -195,8 +195,8 @@ def upload_post():
     return redirect(url_for('upload'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port = FLASK_RUN_PORT)
-
+    app.run()
+    
 
 def create_app():
     app = Flask(__name__)
